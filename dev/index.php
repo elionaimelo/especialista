0ba6c2da6c89c7e8ec49b439761690178bba6224
elionaimelo <elionaimelo.melo@gmail.com>
2015-09-16 17:51:31 -0300

    Versionando especialista

---
+++ dev/index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Página inicial — Cadastro Nacional de Especialistas</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <script src="assets/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body class="index">
+        <!--[if lt IE 7]>
+            <p class="browsehappy">Seu navegador está <strong>desatualizado</strong>. Por favor <a href="http://browsehappy.com/">atualize seu navegador</a> para uma melhor navegação</p>
+        <![endif]-->
+
+
+      <header>
+          <div id="barra-brasil" style="background:#7F7F7F; height: 20px; padding:0 0 0 10px;display:block;">
+            <ul id="menu-barra-temp" style="list-style:none;">
+              <li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED"><a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a></li>
+              <li><a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a></li>
+            </ul>
+          </div>
+        <section class="top">
+          <div class="container">
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">
+                    Ir para o conteúdo
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Ir para o menu
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Ir para a busca
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Ir para o rodapé
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+              <ul id="portal-siteactions"><li id="siteaction-accessibility"><a href="#" title="Acessibilidade" accesskey="5">Acessibilidade</a></li>
+                  <li id="siteaction-contraste"><a href="#" title="Alto Contraste" accesskey="6">Alto Contraste</a></li>
+                  <li id="siteaction-mapadosite" class="last-item"><a href="#" title="Mapa do Site" accesskey="7">Mapa do Site</a></li>
+              </ul>
+
+              <div id="logo">
+                  <a id="portal-logo" title="Secretaria da Receita Federal do Brasil" href="#">
+                          <span id="portal-title-1">Sistema</span>
+                          <h1 id="portal-title" class="luongo">Cadastro Nacional de Especialistas</h1>
+                  </a>
+              </div><!-- ./logo -->
+          </div>
+        </section><!-- ./top -->
+        <section class="sub-top">
+            <ul>
+              <li>
+                Sobre
+              </li>
+              <li>Como acessar</li>
+              <li>
+                Legislação
+              </li>
+              <li>
+                Ajuda
+              </li>
+            </ul>
+
+        </section><!-- ./sub-top -->
+      </header>
+
+      <main>
+
+        <div class="container">
+            <div id="viewlet-above-content">
+              <div id="portal-breadcrumbs">
+                <span id="breadcrumbs-you-are-here">Você está aqui:</span>
+                <span id="breadcrumbs-home">
+                    <a href="#">Página Inicial</a>
+                </span>
+              </div><!-- ./portal-breadcrumbs -->
+            </div><!-- ./viewlet-above-content -->
+
+            <div class="col-2 remove-margin">
+                <div>
+                  <h2 class="title-header">
+                    Bem vindo
+                  </h2>
+
+                  <p>
+                    Nullam in tortor quis felis vestibulum efficitur ut nec lectus. Praesent sed justo rhoncus, facilisis enim vitae, convallis sem. Morbi eros justo, sagittis ac ultrices vel, efficitur et magna. Donec sodales cursus ipsum. Pellentesque eu porttitor sapien. Praesent convallis auctor nisi, in volutpat eros condimentum ac. Nulla vitae lacus sed justo commodo blandit. Donec felis dui, sodales ut venenatis id, dapibus sit amet ex. Mauris nec pellentesque dolor. Aliquam luctus nibh in nibh cursus, ut molestie lorem pulvinar. Nulla vitae finibus lorem. Proin cursus scelerisque vehicula.
+                  </p>
+                </div>
+
+                <form class="busca-geral" action="#" method="post">
+                      <div class="col-1 espace-right">
+                        <h2 class="title-header">
+                          Buscar médico
+                        </h2>
+
+                        <div class="form-group mt-form-group">
+                                   <input type="text" class="form-control" id="search" placeholder="Pesquisar nome, especialidade, CPF ou CRM-UF">
+                               </div>
+                               <div class="form-group">
+                                 <select class="form-control">
+                                   <option>Selecione o Estado</option>
+                                   <option>2</option>
+                                   <option>3</option>
+                                   <option>4</option>
+                                   <option>5</option>
+                                 </select>
+                               </div>
+                               <div class="form-group">
+                                 <select class="form-control">
+                                   <option>Selecione o Município</option>
+                                   <option>2</option>
+                                   <option>3</option>
+                                   <option>4</option>
+                                   <option>5</option>
+                                 </select>
+                               </div>
+
+                               <div class="form-group">
+                                 <select class="form-control">
+                                   <option>Selecione a Especialidade</option>
+                                   <option>2</option>
+                                   <option>3</option>
+                                   <option>4</option>
+                                   <option>5</option>
+                                 </select>
+                               </div>
+                               <button type="submit" class="btn btn-primary">Buscar</button>
+                      </div><!-- ./col-1 -->
+                      <div class="col-1">
+                        <h2 class="title-header">
+                          Buscar especialidade
+                        </h2>
+                        <div class="form-group mt-form-group">
+                                   <input type="text" class="form-control" id="search" placeholder="Pesquisar especialidade">
+                               </div>
+                               <div class="form-group">
+                                 <select class="form-control">
+                                   <option>Selecione o Estado</option>
+                                   <option>2</option>
+                                   <option>3</option>
+                                   <option>4</option>
+                                   <option>5</option>
+                                 </select>
+                               </div>
+                               <div class="form-group">
+                                 <select class="form-control">
+                                   <option>Selecione o Município</option>
+                                   <option>2</option>
+                                   <option>3</option>
+                                   <option>4</option>
+                                   <option>5</option>
+                                 </select>
+                               </div>
+
+                               <button type="submit" class="btn btn-primary">Buscar</button>
+                      </div><!-- ./col-1 -->
+
+                </form>
+
+                <img src="assets/img/banner-forca.png" alt="" class="banner">
+
+
+            </div> <!-- ./col-2 -->
+
+            <aside class="espace-left">
+              <a href="">
+                <img src="assets/img/img-acesse.png" alt="">
+              </a>
+
+              <div class="tile lista-vertical" data-tile="@@collective.cover.collection/b38ac6bb1691418f8c635b31a6eab846" id="b38ac6bb1691418f8c635b31a6eab846">
+
+                      <div class="cover-collection-tile">
+
+                              <div class="tile-header">
+                                  <span>Últimos textos</span>
+                              </div>
+
+                              <div class="collection-item">
+
+                                                  <h3>
+                                                      <a href="http://portalpadrao.gov.br/conteudos-de-marcacao/saiba-como-usar-o-recurso-de-pagina-de-destaque-para-explorar-uma-campanha-institucional" title="Recurso permite criar página com identidade visual própria desvinculada do portal em que estiver hospedada para realçar as mensagens institucionais, de utilidade pública e para ampliar a divulgação de um tema ou evento de caráter temporário">Saiba como usar a Página de Destaque para explorar uma campanha institucional</a>
+                                                  </h3>
+
+                                  <div class="visualClear"><!-- --></div>
+                              </div>
+                              <div class="collection-item">
+
+                                                  <h3>
+                                                      <a href="http://portalpadrao.gov.br/conteudos-de-marcacao/identidade-padrao-de-comunicacao-digital-do-governo-federal" title="Fique por dentro dos elementos e produtos que compõem a Identidade Padrão de Comunicação Digital do Governo Federal">Identidade Padrão de Comunicação Digital do Governo Federal</a>
+                                                  </h3>
+
+
+                                  <div class="visualClear"><!-- --></div>
+                              </div>
+                              <div class="collection-item">
+
+                                                  <h3>
+                                                      <a href="http://portalpadrao.gov.br/conteudos-de-marcacao/conheca-os-templates-para-informativos-da-identidade-padrao" title="Modelos podem ser utilizados para comunicação institucional via mailing">Conheça os templates para informativos da Identidade Padrão </a>
+                                                  </h3>
+
+
+                                  <div class="visualClear"><!-- --></div>
+                              </div>
+                              <div class="collection-item">
+
+                                                  <h3>
+                                                      <a href="http://portalpadrao.gov.br/conteudos-de-marcacao/saiba-mais-sobre-o-portal-padrao-institucional-de-referencia-do-governo-federal" title="O modelo permite que o portal de cada órgão seja reconhecido como propriedade digital do Governo Federal">Saiba mais sobre o Portal Padrão Institucional de Referência do Governo Federal</a>
+                                                  </h3>
+
+                                  <div class="visualClear"><!-- --></div>
+                              </div>
+                              <div class="collection-item">
+
+                                                  <h3>
+                                                      <a href="http://portalpadrao.gov.br/conteudos-de-marcacao/saiba-como-montar-o-menu-da-lei-de-acesso-a-informacao" title="Órgãos do governo federal devem disponibilizar em seu site um menu especificado pela LAI">Saiba como montar o menu da Lei de Acesso à Informação</a>
+                                                  </h3>
+
+                                  <div class="visualClear"><!-- --></div>
+                              </div>
+
+                              <div class="tile-footer">
+                                  <a href="http://portalpadrao.gov.br/conteudos-de-marcacao/ultimas-noticias">acesse a lista completa</a>
+                              </div>
+
+                              <div class="visualClear"><!-- --></div>
+
+                      </div>
+                  </div>
+            </aside>
+
+        </div><!-- ./container -->
+
+      </main>
+
+
+
+
+      <footer>
+        <div class="content">
+            <div class="container">
+                  MAPA DO SITE
+            </div>
+
+        </div>
+        <div id="footer-brasil" class="footer-logos">
+            <div>
+                <a href="http://www.acessoainformacao.gov.br/" class="logo-acesso">
+                    <img src="/++theme++azul/img/acesso-a-infornacao.png" alt="Acesso a Informação"></a>
+                <a href="http://www.brasil.gov.br/" class="logo-brasil">
+                    <img src="/++theme++azul/img/brasil.png" alt="Brasil - Governo Federal"></a>
+            </div>
+        </div>
+        <div class="extra-footer">
+            <p>
+            Desenvolvido com o CMS de código aberto
+            <a href="http://plone.org">Plone</a>
+            </p>
+        </div>
+      </footer>
+
+
+      <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+      <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+      <script src="assets/js/vendor/bootstrap.min.js"></script>
+        <script src="assets/js/scripts.min.js"></script>
+
+        <script defer="defer" src="//barra.brasil.gov.br/barra.js" type="text/javascript"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
